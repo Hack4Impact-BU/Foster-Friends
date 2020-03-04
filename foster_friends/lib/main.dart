b583d5dfb614d0f8ca540a746749b63e8de533bc
jimenezh <jimenezh@bu.edu>
2020-03-04 16:00:17 -0500

    Seperated landing page

--- foster_friends/lib/main.dart
+++ foster_friends/lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import './login.dart';
+import './landing.dart';
 
 void main() => runApp(MaterialApp(
   title: 'Navigation',
@@ -14,56 +15,3 @@ void main() => runApp(MaterialApp(
   }
 ));
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Foster Friends',
-        home: Container(
-          // padding: EdgeInsets.,
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 0.0),
-                child: Center(
-                    child: Text('Foster Friends',
-                        style: TextStyle(
-                            fontSize: 50,
-                            color: Colors.red,
-                            decoration: TextDecoration.none))),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Center(
-                    child: RaisedButton(
-                        onPressed: () {
-                          print("Pressed Log In");
-                          Navigator.pushNamed(  context, '/LogIn');
-                          },
-                        child: Text("Log In",
-                            style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold)),
-                        color: Colors.black)),
-              ),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Center(
-                      child: RaisedButton(
-                          onPressed: () {print("Pressed Sign up");},
-                          child: Text("Sign Up",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold)),
-                          color: Colors.red))),
-            ],
-          ),
-          color: Colors.white,
-        ));
-  }
-}
