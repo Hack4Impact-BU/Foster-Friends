889e2980136e52195ec94880651b689231ccd11d
jimenezh <jimenezh@bu.edu>
2020-03-04 14:04:57 -0500

    Fixed padding by wrapping children of rows in padding widgets

--- foster_friends/lib/main.dart
+++ foster_friends/lib/main.dart
@@ -10,47 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Foster Friends',
-      home: Container(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text('Foster Friends', 
-                style: TextStyle(
-                  fontSize: 50,
-                  color: Colors.red,
-                  decoration: TextDecoration.none
-                ))
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Center(child: RaisedButton(
-                  onPressed: () {},
-                  child: Text("Log In", style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white
-                  )),
-                  color: Colors.black
-                  )),
-                Center(child: RaisedButton(
-                  onPressed: () {},
-                  child: Text("Sign Up", style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white
-                  )),
-                  color: Colors.red
-                  ))
-              ],
-            )
-          ],
-        ),
-        color: Colors.white,
-      )
-    );
+        title: 'Foster Friends',
+        home: Container(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 40.0),
+                child: Center(
+                    child: Text('Foster Friends',
+                        style: TextStyle(
+                            fontSize: 50,
+                            color: Colors.red,
+                            decoration: TextDecoration.none))),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: Center(
+                        child: RaisedButton(
+                            onPressed: () {},
+                            child: Text("Log In",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white)),
+                            color: Colors.black)),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Center(
+                          child: RaisedButton(
+                              onPressed: () {},
+                              child: Text("Sign Up",
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white)),
+                              color: Colors.red))),
+                ],
+              )
+            ],
+          ),
+          color: Colors.white,
+        ));
   }
 }
