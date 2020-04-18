f8883057613cf73f86472d8637db9b72e0fb3cdf
jimenezh <jimenezh@bu.edu>
2020-04-18 18:25:01 -0400

    Clean up

--- foster_friends/lib/formMethods.dart
+++ foster_friends/lib/formMethods.dart
@@ -2,21 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:foster_friends/authentication.dart';
 
-
 void pushIndividualProfile(String userID, String phoneNumber, String email,
     String location, String name) async {
+  DocumentReference ref =
+      Firestore.instance.collection("individuals").document(userID);
 
-    DocumentReference ref =
-        Firestore.instance.collection("individuals").document(userID);
-
-    await ref.setData({
-      "email": email,
-      "phone number": phoneNumber,
-      "location": location,
-      "name": name
-    });
-    print("User profile submitted");
-
+  await ref.setData({
+    "email": email,
+    "phone number": phoneNumber,
+    "location": location,
+    "name": name
+  });
+  print("User profile submitted");
 }
 
 void pushOrganizationProfile(String userID, String address, String description,
@@ -36,18 +33,16 @@ void pushOrganizationProfile(String userID, String address, String description,
 }
 
 Future<bool> existsInDatabase() async {
-
   FirebaseUser user = await getCurrentUser();
   final String uid = user.uid;
 
   DocumentReference refInd =
-        Firestore.instance.collection('individuals').document(uid);
+      Firestore.instance.collection('individuals').document(uid);
   DocumentSnapshot docInd = await refInd.get();
 
   DocumentReference refOrg =
-        Firestore.instance.collection('organizations').document(uid);
+      Firestore.instance.collection('organizations').document(uid);
   DocumentSnapshot docOrg = await refOrg.get();
-
 
   bool both = docOrg.exists && docInd.exists;
 
