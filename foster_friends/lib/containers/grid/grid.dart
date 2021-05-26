c3bbcd0144ffdad2a8568013de96cbbeabbd6e9d
Cici Chen <cicichen@Cicis-MacBook-Pro.local>
2021-05-26 08:03:55 -0400

    updated fixes

--- foster_friends/lib/containers/grid/grid.dart
+++ foster_friends/lib/containers/grid/grid.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:foster_friends/containers/profiles/pets/pet_profile.dart';
 import 'package:foster_friends/state/appState.dart';
 
-Widget buildGrid(List<Map<String, dynamic>> posts, BuildContext context, Map<String, dynamic> user) => GridView.count(
-    crossAxisCount: 3,
-    scrollDirection: Axis.vertical,
-    padding: const EdgeInsets.all(4),
-    children: buildGridTileList(posts, context, user));
+Widget buildGrid(List<Map<String, dynamic>> posts, BuildContext context,
+        Map<String, dynamic> user) =>
+    GridView.count(
+        crossAxisCount: 3,
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.all(4),
+        children: buildGridTileList(posts, context, user));
 
-List<Widget> buildGridTileList(List<Map<String, dynamic>> posts, BuildContext context, Map<String, dynamic> user) {
+List<Widget> buildGridTileList(List<Map<String, dynamic>> posts,
+    BuildContext context, Map<String, dynamic> user) {
   if (posts.length < 1) {
     return new List(0);
   }
@@ -19,8 +22,7 @@ List<Widget> buildGridTileList(List<Map<String, dynamic>> posts, BuildContext co
         child: Container(
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: NetworkImage(posts[i]['image']),
-                    fit: BoxFit.cover),
+                    image: NetworkImage(posts[i]['image']), fit: BoxFit.cover),
                 borderRadius: BorderRadius.all(Radius.circular(100))),
             child: FlatButton(
               child: null,
@@ -34,7 +36,10 @@ List<Widget> buildGridTileList(List<Map<String, dynamic>> posts, BuildContext co
                 store.dispatch(getFirebaseUser);
                 // print(store.state.userPetData);
                 // print(user);
-                showDialog(context: context, builder: (BuildContext context) => PetProfile(posts[i], user));
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        PetProfile(posts[i], user));
               },
             )),
       ),
